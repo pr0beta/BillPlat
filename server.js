00414efa52f1ff96cfe9e1cca80f4aba04a08587
pr0beta <zach@zacharycancio.com>
2011-03-18 12:12:34 -0400

    initial secure server.  add .gitignore.  add initial index.jade view

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-hello world
+// required modules
+var express = require('express');
+var express_resource = require('express-resource');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var io= require('socket.io');
+var crypto = require('crypto');
+
+var options = {
+ key: fs.readFileSync('privatekey.pem').toString(),
+ cert: fs.readFileSync('certificate.pem').toString()
+}
+
+var app = express.createServer(options);
+// var app = express.createServer();
+
+// initialization
+app.set('view options', {
+  layout: false
+})
+
+app.get('/', function(req, res){
+  res.render('index.jade', {title: 'My Site'});
+});
+
+app.listen(3000);
